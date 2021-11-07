48bd9a65732d8f45dd76d4d1a2fe1d2b5a316bec
Evgeny Gorbovoy <jinek@msn.com>
2021-11-08 01:37:02 +0300

    Caret is now placed at correct positions. CaretControl is fixed. Caret in TextBox is fixed. Caret is now hiden if out of clip.

--- src/Consolonia.Core/Infrastructure/DefaultNetConsole.cs
+++ src/Consolonia.Core/Infrastructure/DefaultNetConsole.cs
@@ -6,12 +6,114 @@ namespace Consolonia.Core.Infrastructure
 {
     internal class DefaultNetConsole : IConsole
     {
+        private (ConsoleColor background, ConsoleColor foreground, char character)?[,] _cache;
+        private object _currentControlOfCaret;
+        private ConsoleColor _headBackground;
+        private ConsoleColor _headForeground;
+        private (ushort x, ushort y) _headPosition;
+        private bool _caretVisible;
+
         public DefaultNetConsole()
         {
             Console.CursorVisible = false;
             InitializeCache();
             StartSizeCheckTimer();
         }
+
+
+        public void MoveCaretForControl(Point? position, int size, object ownerControl)
+        {
+            if (_currentControlOfCaret != ownerControl)
+                throw new InvalidOperationException();
+
+            //todo: must be int top,left instead of position
+            // Console.CursorSize = size; todo: not supported on linux
+            if (position == null)
+                HideCaretInternal();
+            else
+            {
+                ShowCaretInternal();
+                (double x, double y) = (Point)position;
+                SetCaretPosition(((ushort)x, (ushort)y));
+            }
+        }
+
+        public void AddCaretFor(object control)
+        {
+            if (_currentControlOfCaret != null)
+                throw new NotSupportedException();
+
+            _currentControlOfCaret = control;
+
+            ShowCaretInternal();
+        }
+
+        private void ShowCaretInternal()
+        {
+            if (_caretVisible)
+                return;
+            _caretVisible = Console.CursorVisible = true;
+        }
+
+        public void RemoveCaretFor(object control)
+        {
+            if (_currentControlOfCaret == null)
+                throw new NotSupportedException();
+
+            _currentControlOfCaret = null;
+            HideCaretInternal();
+        }
+
+        private void HideCaretInternal()
+        {
+            if (!_caretVisible)
+                return;
+            _caretVisible = Console.CursorVisible = false;
+        }
+
+        public IDisposable StoreCaret()
+        {
+            return new CaretStorage(this);
+        }
+
+        public void SetCaretPosition((ushort x, ushort y) position)
+        {
+            (ushort left, ushort top) = GetCaretPosition();
+            (ushort x, ushort y) = position;
+            if (left == x && top == y) return;
+            _headPosition = (x, y);
+            Console.SetCursorPosition(x, y);
+        }
+
+        public (ushort x, ushort y) GetCaretPosition()
+        {
+            return _headPosition;
+        }
+
+        public void Print(ushort x, ushort y, ConsoleColor backgroundColor, ConsoleColor foregroundColor,
+            char character)
+        {
+            SetCaretPosition((x, y));
+
+            if (_headBackground != backgroundColor)
+                _headBackground = Console.BackgroundColor = backgroundColor;
+            if (_headForeground != foregroundColor)
+                _headForeground = Console.ForegroundColor = foregroundColor;
+
+            if (_cache[x, y] == (backgroundColor, foregroundColor, character))
+                return;
+
+            _cache[x, y] = (backgroundColor, foregroundColor, character);
+            Console.Write(character);
+            if (_headPosition.x < Size.width - 1)
+                _headPosition.x++;
+            else _headPosition = ((ushort x, ushort y))(0, _headPosition.y + 1);
+        }
+
+        public event Action? Resized;
+
+        public (ushort width, ushort height) Size =>
+            ((ushort width, ushort height))(Console.WindowWidth, Console.WindowHeight);
 
         private async void StartSizeCheckTimer()
         {
@@ -46,85 +148,9 @@ namespace Consolonia.Core.Infrastructure
             }
         }
 
-
-        public void MoveCaretForControl(Point position, int size, object ownerControl)
-        {
-            //todo: must be int top,left instead of position
-            //todo: check if ownerControl is current caret showing, otherwise exception
-            // Console.CursorSize = size; todo: not supported on linux
-            SetCaretPosition(((ushort)position.X, (ushort)position.Y));
-        }
-
-        private object _carrentControlOfCaret;
-
-        public void AddCaretFor(object control)
-        {
-            /*todo: if (_carrentControlOfCaret != null)
-                throw new NotSupportedException();*/
-            _carrentControlOfCaret = control;
-            Console.CursorVisible = true;
-        }
-
-        public void RemoveCaretFor(object control)
-        {
-            /*todo: if (_carrentControlOfCaret == null)
-                throw new NotSupportedException(); */
-            _carrentControlOfCaret = null;
-            Console.CursorVisible = false;
-        }
-
-        public IDisposable StoreCaret()
-        {
-            return new CaretStorage(this);
-        }
-
-        public void SetCaretPosition((ushort x, ushort y) position)
-        {
-            (ushort left, ushort top) = GetCaretPosition();
-            (ushort x, ushort y) = position;
-            if (left == x && top == y) return;
-            _headPosition = (x, y);
-            Console.SetCursorPosition(x, y);
-        }
-
-        private (ushort x, ushort y) _headPosition;
-        private ConsoleColor _headBackground;
-        private ConsoleColor _headForeground;
-        private (ConsoleColor background, ConsoleColor foreground, char character)?[,] _cache;
-
         private void InitializeCache()
         {
             _cache = new (ConsoleColor background, ConsoleColor foreground, char character)?[Size.width, Size.height];
         }
-
-        public (ushort x, ushort y) GetCaretPosition()
-        {
-            return _headPosition;
-        }
-
-        public void Print(ushort x, ushort y, ConsoleColor backgroundColor, ConsoleColor foregroundColor,
-            char character)
-        {
-            SetCaretPosition((x, y));
-
-            if (_headBackground != backgroundColor)
-                _headBackground = Console.BackgroundColor = backgroundColor;
-            if (_headForeground != foregroundColor)
-                _headForeground = Console.ForegroundColor = foregroundColor;
-
-            if (_cache[x, y] == (backgroundColor, foregroundColor, character))
-                return;
-
-            _cache[x, y] = (backgroundColor, foregroundColor, character);
-            Console.Write(character);
-            if (_headPosition.x < Size.width - 1)
-                _headPosition.x++;
-            else _headPosition = ((ushort x, ushort y))(0, _headPosition.y + 1);
-        }
-
-        public event Action? Resized;
-
-        public (ushort width, ushort height) Size =>
-            ((ushort width, ushort height))(Console.WindowWidth, Console.WindowHeight);
     }
 }
