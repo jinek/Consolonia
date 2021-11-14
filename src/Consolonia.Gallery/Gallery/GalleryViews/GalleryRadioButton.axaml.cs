d09848fc2580d51ea92f92acc1c40bf917b983b5
Evgeny Gorbovoy <jinek@msn.com>
2021-11-14 05:09:58 +0300

    Radio button (#45)
    * Gallery application added
    
    Additionally:
    * TextBlock is done (with trimming, wrapping etc)
    * Resizing the window is more responsive
    * UserControl template added
    * DataGrid is focusable by default
    * TextShaperImpl implemented
    
    * FourBitColorBrush is now possible to use as markup extension
    
    * New basic color resource introduced
    
    Additionally:
    * templates of controls adjusted
    * pen in rectangle now can be null
    
    * Caret is now placed at correct positions.
    CaretControl is fixed.
    Caret in TextBox is fixed.
    Caret is now hiden if out of clip.
    
    * Few minor fixes:
    
    * ScrolLViewer is now transparent
    * TextBox theme adjusted
    * TextBlock example fixed
    *
    
    * Button template is done
    
    * Additionally text/key input is modified (was needed for a button to get spacebar press)
    * Check of MoveCaretForControl has been removed as it does not work as expected for correct input
    * Introduced SymbolsControl to draw text primitives (more lightweight than TextBlock)
    * Button example added to gallery + RepeatButton and ToggleButton which still don't work
    
    * CheckBox template implemented.
    
    Additionally:
    * CheckBox gallery item added
    
    * ComboBox template
    
    * hack to focus combobox
    
    * ComboBox template and behavior
    
    Additionally
    * ComboBox gallery page added
    * Gallery item pre-selection fixed
    * visual brush rendering
    * LightDismissOverlaylayer style
    * Panel style
     scrollBar/ScrollViewer styles adjusted
    
    * TextBox template
    
    * FormattedText implementation
    * FormattedText can have several brushes
    * fixed issue of caret be shown while rendering to console
    * ScrollBar fixed in order to draw multiline TextBox+horizontal scrollbar+scrollviewer
    * DataValidationErrors template
    * TextBox gallery case added
    
    * ScrollViewer gallery item
    
    * ScrollViewer tepmlate updated. ScrollBar can be manipulated with arrow keys when is focusable
    
    * ScrollViewer template updated
    
    * DataGrid template semi-done
    
    Additionally:
    * TabControl template done
    * CaretControl fixed
    * DataGrid gallery case added
    * TabControl added to gallery
    
    * RadioButton template and gallery

---
+++ src/Consolonia.Gallery/Gallery/GalleryViews/GalleryRadioButton.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Consolonia.Gallery.Gallery.GalleryViews
+{
+    public class GalleryRadioButton : UserControl
+    {
+        public GalleryRadioButton()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
