71d688eb5dc4a2022beadd5c9d5c6d2d0e386b6c
Evgeny Gorbovoy <jinek@msn.com>
2021-11-14 05:00:04 +0300

    FourBitColorBrush is now possible to use as markup extension (#21)

--- src/Consolonia.Core/Drawing/FourBitColorBrush.cs
+++ src/Consolonia.Core/Drawing/FourBitColorBrush.cs
@@ -7,17 +7,31 @@ namespace Consolonia.Core.Drawing
 {
     public class FourBitColorBrush : Brush
     {
+        public static readonly StyledProperty<ConsoleColor> ColorProperty =
+            AvaloniaProperty.Register<FourBitColorBrush, ConsoleColor>(nameof(Color));
+
+        public static readonly StyledProperty<PixelBackgroundMode> ModeProperty =
+            AvaloniaProperty.Register<FourBitColorBrush, PixelBackgroundMode>(nameof(Mode));
+
         static FourBitColorBrush()
         {
             AffectsRender<FourBitColorBrush>(ColorProperty);
             AffectsRender<FourBitColorBrush>(ModeProperty);
         }
 
-        public static readonly StyledProperty<ConsoleColor> ColorProperty =
-            AvaloniaProperty.Register<FourBitColorBrush, ConsoleColor>(nameof(Color));
+        public FourBitColorBrush(ConsoleColor consoleColor, PixelBackgroundMode mode) : this(consoleColor)
+        {
+            Mode = mode;
+        }
 
-        public static readonly StyledProperty<PixelBackgroundMode> ModeProperty =
-            AvaloniaProperty.Register<FourBitColorBrush, PixelBackgroundMode>(nameof(Mode));
+        public FourBitColorBrush(ConsoleColor consoleColor)
+        {
+            Color = consoleColor;
+        }
+
+        public FourBitColorBrush()
+        {
+        }
 
         public PixelBackgroundMode Mode
         {
@@ -31,8 +45,17 @@ namespace Consolonia.Core.Drawing
             set => SetValue(ColorProperty, value);
         }
 
+        // ReSharper disable once UnusedMember.Global used by Avalonia
+        // ReSharper disable once UnusedParameter.Global
+        public Brush ProvideValue(IServiceProvider _)
+        {
+            return this;
+        }
+
+
         public override IBrush ToImmutable()
         {
+            //todo: implement immutable
             return new FourBitColorBrush { Color = Color, Mode = Mode };
         }
     }
