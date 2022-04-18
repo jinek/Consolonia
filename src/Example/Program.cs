d700c9a609fe640b4cfedc7ac67b93f06c48e15b
Evgeny Gorbovoy <e.gorbovoy@staffwerke.de>
2022-04-18 05:34:59 +0200

    Startup methods added/re-arranged

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -1,12 +1,18 @@
+using Avalonia;
 using Consolonia.Core;
 
 namespace Example
 {
     internal static class Program
     {
-        public static void Main()
+        public static int Main()
         {
-            ApplicationStartup.StartConsolonia<App>();
+            return AppBuilder.Configure<App>()
+                .UseConsolonia()
+                .UseStandardConsole()
+                .LogToTrace()
+                .StartWithConsoleLifetime(null);
+            
         }
     }
 }
