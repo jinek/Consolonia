0f1048eba2b4fca0ab03bfc7a6fb4e53ae1fff75
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-10-31 23:18:39 -0700

    Clear screen and don't redraw while we are actively resizing. This gets rid of a ton of visual artifiacts while resizing and also is more efficient as we are not redrawing the screen constantly.

--- src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
+++ src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -19,6 +21,8 @@ namespace Consolonia.Core.Infrastructure
         private readonly IKeyboardDevice _myKeyboardDevice;
         [NotNull] internal readonly IConsole Console;
         private IInputRoot _inputRoot;
+        private CancellationTokenSource? _resizeCancellationTokenSource;
+        private readonly TimeSpan _resizeDelay = TimeSpan.FromMilliseconds(50);
 
         public ConsoleWindow()
         {
@@ -295,11 +299,33 @@ namespace Consolonia.Core.Infrastructure
 
         private void OnConsoleOnResized()
         {
-            Dispatcher.UIThread.Post(() =>
+            // clear screen so we don't see crazy while resizing.
+            System.Console.Clear();
+
+            // Cancel previous task if there is one and start a new one
+            _resizeCancellationTokenSource?.Cancel();
+            _resizeCancellationTokenSource = new CancellationTokenSource();
+
+            // start a task which if no resize event comes for _resizeDelay will post the resize to the window
+            Task.Run(async () =>
             {
-                PixelBufferSize pixelBufferSize = Console.Size;
-                var size = new Size(pixelBufferSize.Width, pixelBufferSize.Height);
-                Resized!(size, WindowResizeReason.Unspecified);
+                try
+                {
+                    // Wait for the delay period, this task will be canceled if another refresh comes in.
+                    await Task.Delay(_resizeDelay, _resizeCancellationTokenSource.Token).ConfigureAwait(false);
+
+                    // dispatch to the UI thread 
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        PixelBufferSize pixelBufferSize = Console.Size;
+                        var size = new Size(pixelBufferSize.Width, pixelBufferSize.Height);
+                        Resized!(size, WindowResizeReason.Unspecified);
+                    });
+                }
+                catch (TaskCanceledException)
+                {
+                    // Ignore cancellation exception, we want this to happen while resizes are happening quickly
+                }
             });
         }
 
