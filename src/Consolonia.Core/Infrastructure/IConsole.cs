78710e5b96360b57ad3382f1674ab38d54a72384
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-11-08 09:54:03 -0800

    add comment

--- src/Consolonia.Core/Infrastructure/IConsole.cs
+++ src/Consolonia.Core/Infrastructure/IConsole.cs
@@ -32,6 +32,9 @@ namespace Consolonia.Core.Infrastructure
         void PauseIO(Task task);
         void ClearOutput();
 
+        /// <summary>
+        /// This is true if console supports composing multiple emojis together (like: 👨‍👩‍👧‍👦).
+        /// </summary>
         bool SupportsComplexEmoji { get; }
     }
 }
