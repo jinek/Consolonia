9dd81a916088297a49d9392469bf37863bdabe59
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-11-08 13:58:32 -0800

    remove changes

--- src/Consolonia.Core/Infrastructure/ConsoloniaLifetime.cs
+++ src/Consolonia.Core/Infrastructure/ConsoloniaLifetime.cs
@@ -9,21 +9,6 @@ namespace Consolonia.Core.Infrastructure
 {
     public class ConsoloniaLifetime : ClassicDesktopStyleApplicationLifetime
     {
-        public ConsoloniaLifetime()
-        {
-#pragma warning disable CA1303 // Do not pass literals as localized parameters
-            Console.Write(ConsoleUtils.EnableAlternateBuffer);
-#pragma warning restore CA1303 // Do not pass literals as localized parameters
-            Exit += ConsoloniaLifetime_Exit;
-        }
-
-        private void ConsoloniaLifetime_Exit(object sender, ControlledApplicationLifetimeExitEventArgs e)
-        {
-#pragma warning disable CA1303 // Do not pass literals as localized parameters
-            Console.Write(ConsoleUtils.DisableAlternateBuffer);
-#pragma warning restore CA1303 // Do not pass literals as localized parameters
-        }
-
         /// <summary>
         ///     returned task indicates that console is successfully paused
         /// </summary>
@@ -48,7 +33,5 @@ namespace Consolonia.Core.Infrastructure
 
             return Dispatcher.UIThread.InvokeAsync(() => { }).GetTask();
         }
-
-
     }
 }
