ee365614726ea579b8d36fd53ecad71d5c234b77
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-02 16:40:26 -0800

    cleanup

--- src/Example.Blazor/App.axaml.cs
+++ src/Example.Blazor/App.axaml.cs
@@ -1,17 +1,9 @@
-using Avalonia;
-using Avalonia.Markup.Xaml;
 using BlazorBindingsAvalonia;
 using Example.Blazor.Components;
 
 namespace Example.Blazor
 {
-
-
     public class App : BlazorBindingsApplication<MainPage>
     {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
     }
 }
