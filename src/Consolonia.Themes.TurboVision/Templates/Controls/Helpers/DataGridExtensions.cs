9aff4c9a8fcf23651f00d1036cf4a18e450d4f3c
Evgeny Gorbovoy <jinek@msn.com>
2024-12-04 21:27:07 +0100

    Something runs with this setup.

--- src/Consolonia.Themes.TurboVision/Templates/Controls/Helpers/DataGridExtensions.cs
+++ src/Consolonia.Themes.TurboVision/Templates/Controls/Helpers/DataGridExtensions.cs
@@ -3,7 +3,7 @@ using Avalonia.Controls;
 
 namespace Consolonia.Themes.TurboVision.Templates.Controls.Helpers
 {
-    internal static class DataGridExtensions
+    public static class DataGridExtensions
     {
         public static readonly AttachedProperty<bool> IsSelectedProperty =
             AvaloniaProperty.RegisterAttached<DataGridRow, bool>("IsSelected", typeof(DataGridExtensions));
