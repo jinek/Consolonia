4cde4c1ab585cbf6619eb372d860faba5195e9c6
Evgeny Gorbovoy <jinek@msn.com>
2024-12-05 15:06:02 +0100

    Datagrid works with this

--- src/Consolonia.Themes.TurboVision/Templates/Controls/Helpers/DataGridExtensions.cs
+++ src/Consolonia.Themes.TurboVision/Templates/Controls/Helpers/DataGridExtensions.cs
@@ -7,5 +7,15 @@ namespace Consolonia.Themes.TurboVision.Templates.Controls.Helpers
     {
         public static readonly AttachedProperty<bool> IsSelectedProperty =
             AvaloniaProperty.RegisterAttached<DataGridRow, bool>("IsSelected", typeof(DataGridExtensions));
+        
+        public static bool GetIsSelected(DataGridRow element)
+        {
+            return element.GetValue(IsSelectedProperty);
+        }
+        
+        public static void SetIsSelected(DataGridRow element, bool value)
+        {
+            element.SetValue(IsSelectedProperty, value);
+        }
     }
 }
