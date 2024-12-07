a256342d822bdfacb02d8b2fdb0b6e5055b3b63c
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-07 11:03:38 -0800

    Prepare build and packages for publishing (#175)
    * add support for NUGET publishing actions
    
     **PublishNuget**
    - builds release and publishes nuget and symbol nuget for the current checked in version
    
     **PublishIncrementNuget**
    - Bumps incremental build number (11.2.X)
    - builds release and publishes nuget and symbol nuget for the
    - Checks in updated build number
    
    NOTE: This needs a NUGET_KEY secret added
    
    * added icon and readme to each package and made sure they build with the right dependencies
    
    * cleanup package references
    
    * clean up package references, GuiCS is internal assembly instead of nuget package
    
    * update readmes
    Add ConsoloniaApplication<MainWindow>
    Cleaned up turbovision stuff
    
    * update readmes, change previewhost gallery to use ConsoloniaApplication<T>
    
    * fixes for merge
    
    * add ability to switch views to gallery
    
    * Update src/Consolonia.Core/Infrastructure/ConsoloniaApplication.cs
    
    * Add consolonia meta package
    
    * rename Consolonia.Themes.TurboVision to Consolonia.Themes
    Move namespaces for thems to Consolonia.Themes
    
    * move themes to root of project to align with namespaces
    
    * switch to using buildprops for icon and readme on packages

---
+++ src/Consolonia.Themes/Templates/Controls/Helpers/SymbolsControl.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Media;
+using Avalonia.Media.TextFormatting;
+using Avalonia.Platform;
+using Consolonia.Core.Helpers;
+
+namespace Consolonia.Themes.Templates.Controls.Helpers
+{
+    /// <summary>
+    ///     Represents a control that displays symbols.
+    ///     This control has two modes of operation depending on the Fill property.
+    ///     If the Fill property is false, it just draws <see cref="Text" />
+    ///     If the Fill property is true, the symbol (Text[0]) is repeated and fills the control.
+    /// </summary>
+    public sealed class SymbolsControl : Control, IDisposable
+    {
+        /// <summary>
+        ///     Defines the <see cref="Foreground" /> property.
+        /// </summary>
+        public static readonly StyledProperty<IBrush> ForegroundProperty =
+            TextBlock.ForegroundProperty.AddOwner<SymbolsControl>();
+
+        public static readonly DirectProperty<SymbolsControl, string> TextProperty =
+            AvaloniaProperty.RegisterDirect<SymbolsControl, string>(
+                nameof(Text),
+                o => o.Text,
+                (o, v) => o.Text = v,
+                TextBlock.TextProperty.GetDefaultValue(typeof(TextBlock)),
+                BindingMode.TwoWay);
+
+        public static readonly StyledProperty<bool> FillProperty =
+            AvaloniaProperty.Register<SymbolsControl, bool>(nameof(Fill));
+
+        private GlyphRun _shapedText;
+
+        private string _text;
+
+        static SymbolsControl()
+        {
+            AffectsRender<SymbolsControl>(ForegroundProperty, TextProperty, FillProperty);
+            AffectsMeasure<SymbolsControl>(TextProperty, FillProperty);
+            AffectsArrange<SymbolsControl>(TextProperty, FillProperty);
+        }
+
+        /// <summary>
+        ///     Gets or sets a brush used to paint the text.
+        /// </summary>
+        public IBrush Foreground
+        {
+            get => GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        public bool Fill
+        {
+            get => GetValue(FillProperty);
+            set => SetValue(FillProperty, value);
+        }
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+
+                var platformRender = AvaloniaLocator.Current.GetService<IPlatformRenderInterface>();
+                var textShaper = AvaloniaLocator.Current.GetService<ITextShaperImpl>();
+                var fontManager = AvaloniaLocator.Current.GetService<IFontManagerImpl>();
+                fontManager.TryCreateGlyphTypeface("Cascadia Mono", FontStyle.Normal, FontWeight.Normal,
+                    FontStretch.Normal, out IGlyphTypeface typeface);
+                ArgumentNullException.ThrowIfNull(typeface);
+                ShapedBuffer glyphs =
+                    textShaper.ShapeText(value.AsMemory(),
+                        new TextShaperOptions(typeface, typeface.Metrics.DesignEmHeight));
+                IGlyphRunImpl glyphRunImpl = platformRender.CreateGlyphRun(typeface, 1, glyphs, default);
+                _shapedText = new GlyphRun(glyphRunImpl.GlyphTypeface,
+                    glyphRunImpl.FontRenderingEmSize,
+                    _text.AsMemory(),
+                    glyphs,
+                    default(Point));
+            }
+        }
+
+        public void Dispose()
+        {
+            _shapedText?.Dispose();
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            if (!Fill)
+            {
+                context.DrawGlyphRun(Foreground, _shapedText);
+            }
+            else
+            {
+                if (Text is not { Length: > 0 }) return;
+                // Draw the text as a repeating pattern
+                var formattedText = new FormattedText(
+                    string.Concat(
+                        Enumerable.Repeat(Text, (int)Bounds.Width / Text.MeasureText())),
+                    CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    Typeface.Default, 1, Foreground);
+                for (int y = 0; y < Bounds.Height; y++)
+                    context.DrawText(formattedText, new Point(0, y));
+            }
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return !Fill ? _shapedText?.Bounds.Size ?? default : default;
+        }
+    }
+}
