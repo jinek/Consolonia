b7b4443d3db8f1b15e4c280c63e3503becd96446
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-13 18:20:39 -0800

    refactored based on coderabbit review

--- src/Consolonia.Core/Controls/ConsoloniaAccessText.cs
+++ src/Consolonia.Core/Controls/ConsoloniaAccessText.cs
@@ -9,58 +9,65 @@ namespace Consolonia.Core.Controls
     /// <summary>
     /// A text block that displays a character prefixed with an underscore as an access key.
     /// </summary>
-    public class ConsoloniaAccessText : AccessText
+    public class ConsoloniaAccessText : AccessText, IDisposable
     {
+        private bool _disposed;
         private Run _accessRun;
 
         public ConsoloniaAccessText()
         {
-            this.PropertyChanged += (sender, e) =>
-            {
-                switch (e.Property.Name)
-                {
-                    case nameof(Text):
-                        {
-                            if (!String.IsNullOrEmpty(Text))
-                            {
-                                _accessRun = null;
-                                InlineCollection inlines = new InlineCollection();
-                                var iPos = Text.IndexOf('_', StringComparison.Ordinal);
-                                if (iPos >= 0)
-                                {
-                                    inlines.Add(new Run(Text.Substring(0, iPos)));
-                                    _accessRun = new Run(Text.Substring(++iPos, 1))
-                                    {
-                                        TextDecorations = new TextDecorationCollection()
-                                    };
-                                    inlines.Add(_accessRun);
-                                    inlines.Add(new Run(Text.Substring(iPos + 1)));
-                                }
-                                else
-                                {
-                                    inlines.Add(new Run(Text));
-                                }
-                                this.Inlines = inlines;
-                            }
-                        }
-                        break;
-                    case nameof(ShowAccessKey):
-                        if (_accessRun != null)
-                        {
-                            if (this.ShowAccessKey)
-                                _accessRun.TextDecorations.Add(new TextDecoration { Location = TextDecorationLocation.Underline });
-                            else
-                                _accessRun.TextDecorations.Clear();
-                        }
-                        break;
-                    default:
-                        break;
-                }
-
-            };
-
-            /// <inheritdoc/>
+            this.PropertyChanged += OnPropertyChanged;
         }
 
+        public void Dispose()
+        {
+            this.PropertyChanged -= OnPropertyChanged;
+            _disposed = true;
+        }
+
+        private void OnPropertyChanged(object sender, Avalonia.AvaloniaPropertyChangedEventArgs e)
+        {
+            switch (e.Property.Name)
+            {
+                case nameof(Text):
+                    {
+                        if (!String.IsNullOrEmpty(Text))
+                        {
+                            _accessRun = null;
+                            InlineCollection inlines = new InlineCollection();
+                            var iPos = Text.IndexOf('_', StringComparison.Ordinal);
+                            if (iPos >= 0 && iPos < Text.Length - 1)  
+                            {
+                                inlines.Add(new Run(Text.Substring(0, iPos)));
+                                _accessRun = new Run(Text.Substring(++iPos, 1))
+                                {
+                                    TextDecorations = new TextDecorationCollection()
+                                };
+                                inlines.Add(_accessRun);
+                                inlines.Add(new Run(Text.Substring(iPos + 1)));
+                            }
+                            else
+                            {
+                                inlines.Add(new Run(Text));
+                            }
+                            this.Inlines = inlines;
+                        }
+                    }
+                    break;
+
+                case nameof(ShowAccessKey):
+                    if (_accessRun != null)
+                    {
+                        if (this.ShowAccessKey)
+                            _accessRun.TextDecorations.Add(new TextDecoration { Location = TextDecorationLocation.Underline });
+                        else
+                            _accessRun.TextDecorations.Clear();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
