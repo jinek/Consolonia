02663c450dc2de7595b86cb67ca4aef3cbfa329f
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-13 18:11:33 -0800

    Add ConsoloniaAccessText underline

---
+++ src/Consolonia.Core/Controls/ConsoloniaAccessText.cs
@@ -0,0 +1,66 @@
+using System;
+using Avalonia.Media;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Documents;
+
+namespace Consolonia.Core.Controls
+{
+
+    /// <summary>
+    /// A text block that displays a character prefixed with an underscore as an access key.
+    /// </summary>
+    public class ConsoloniaAccessText : AccessText
+    {
+        private Run _accessRun;
+
+        public ConsoloniaAccessText()
+        {
+            this.PropertyChanged += (sender, e) =>
+            {
+                switch (e.Property.Name)
+                {
+                    case nameof(Text):
+                        {
+                            if (!String.IsNullOrEmpty(Text))
+                            {
+                                _accessRun = null;
+                                InlineCollection inlines = new InlineCollection();
+                                var iPos = Text.IndexOf('_', StringComparison.Ordinal);
+                                if (iPos >= 0)
+                                {
+                                    inlines.Add(new Run(Text.Substring(0, iPos)));
+                                    _accessRun = new Run(Text.Substring(++iPos, 1))
+                                    {
+                                        TextDecorations = new TextDecorationCollection()
+                                    };
+                                    inlines.Add(_accessRun);
+                                    inlines.Add(new Run(Text.Substring(iPos + 1)));
+                                }
+                                else
+                                {
+                                    inlines.Add(new Run(Text));
+                                }
+                                this.Inlines = inlines;
+                            }
+                        }
+                        break;
+                    case nameof(ShowAccessKey):
+                        if (_accessRun != null)
+                        {
+                            if (this.ShowAccessKey)
+                                _accessRun.TextDecorations.Add(new TextDecoration { Location = TextDecorationLocation.Underline });
+                            else
+                                _accessRun.TextDecorations.Clear();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            };
+
+            /// <inheritdoc/>
+        }
+
+    }
+}
