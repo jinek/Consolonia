b5f5f46f2185a0b63555af8c17bb983192687fbc
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-16 18:04:38 -0800

    cleanup unused usings

--- src/Consolonia.Core/Helpers/Observable.cs
+++ src/Consolonia.Core/Helpers/Observable.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Avalonia.Reactive.Operators;
-using Avalonia.Threading;
-using global::Avalonia.Reactive;
+using Avalonia.Reactive;
 
 namespace Consolonia.Core.Helpers
 {
