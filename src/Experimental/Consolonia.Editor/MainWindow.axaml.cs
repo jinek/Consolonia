132eb13a3e9daf1baf84a26912b7b13c3d7b73c3
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-12-29 08:52:51 -0800

    add escape codes

--- src/Experimental/Consolonia.Editor/MainWindow.axaml.cs
+++ src/Experimental/Consolonia.Editor/MainWindow.axaml.cs
@@ -34,7 +34,6 @@ namespace ConsoloniaEdit.Demo
         private OverloadInsightWindow _insightWindow;
         private ComboBox _syntaxModeCombo;
         private TextBlock _statusTextBlock;
-        private ElementGenerator _generator = new ElementGenerator();
         private RegistryOptions _registryOptions;
         private int _currentTheme = (int)ThemeName.DarkPlus;
 
@@ -49,8 +48,6 @@ namespace ConsoloniaEdit.Demo
             _textEditor.Background = Brushes.Transparent;
             _textEditor.ShowLineNumbers = true;
             _textEditor.TextArea.Background = this.Background;
-            _textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
-            _textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
             _textEditor.Options.AllowToggleOverstrikeMode = true;
             _textEditor.Options.EnableTextDragDrop = true;
             _textEditor.Options.ShowBoxForControlCharacters = true;
@@ -58,17 +55,6 @@ namespace ConsoloniaEdit.Demo
             _textEditor.TextArea.IndentationStrategy = new AvaloniaEdit.Indentation.CSharp.CSharpIndentationStrategy(_textEditor.Options);
             _textEditor.TextArea.Caret.PositionChanged += Caret_PositionChanged;
             _textEditor.TextArea.RightClickMovesCaret = true;
-
-            //_addControlButton = this.FindControl<Button>("addControlBtn");
-            //_addControlButton.Click += AddControlButton_Click;
-
-            //_clearControlButton = this.FindControl<Button>("clearControlBtn");
-            //_clearControlButton.Click += ClearControlButton_Click;
-
-            //_insertSnippetButton = this.FindControl<Button>("insertSnippetBtn");
-            //_insertSnippetButton.Click += InsertSnippetButton_Click;
-
-            _textEditor.TextArea.TextView.ElementGenerators.Add(_generator);
 
             _registryOptions = new RegistryOptions(
                 (ThemeName)_currentTheme);
@@ -234,196 +220,7 @@ namespace ConsoloniaEdit.Demo
             }
         }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+  
 
-        private void textEditor_TextArea_TextEntering(object sender, TextInputEventArgs e)
-        {
-            if (e.Text.Length > 0 && _completionWindow != null)
-            {
-                if (!char.IsLetterOrDigit(e.Text[0]))
-                {
-                    // Whenever a non-letter is typed while the completion window is open,
-                    // insert the currently selected element.
-                    _completionWindow.CompletionList.RequestInsertion(e);
-                }
-            }
-
-            _insightWindow?.Hide();
-
-            // Do not set e.Handled=true.
-            // We still want to insert the character that was typed.
-        }
-
-        private void textEditor_TextArea_TextEntered(object sender, TextInputEventArgs e)
-        {
-            if (e.Text == ".")
-            {
-
-                _completionWindow = new CompletionWindow(_textEditor.TextArea);
-                _completionWindow.Closed += (o, args) => _completionWindow = null;
-
-                var data = _completionWindow.CompletionList.CompletionData;
-
-                for (int i = 0; i < 500; i++)
-                {
-                    data.Add(new MyCompletionData("Item" + i.ToString()));
-                }
-
-                data.Insert(20, new MyCompletionData("long item to demosntrate dynamic poup resizing"));
-
-                _completionWindow.Show();
-            }
-            else if (e.Text == "(")
-            {
-                _insightWindow = new OverloadInsightWindow(_textEditor.TextArea);
-                _insightWindow.Closed += (o, args) => _insightWindow = null;
-
-                _insightWindow.Provider = new MyOverloadProvider(new[]
-                {
-                    ("Method1(int, string)", "Method1 description"),
-                    ("Method2(int)", "Method2 description"),
-                    ("Method3(string)", "Method3 description"),
-                });
-
-                _insightWindow.Show();
-            }
-        }
-
-
-        private class MyOverloadProvider : IOverloadProvider
-        {
-            private readonly IList<(string header, string content)> _items;
-            private int _selectedIndex;
-
-            public MyOverloadProvider(IList<(string header, string content)> items)
-            {
-                _items = items;
-                SelectedIndex = 0;
-            }
-
-            public int SelectedIndex
-            {
-                get => _selectedIndex;
-                set
-                {
-                    _selectedIndex = value;
-                    OnPropertyChanged();
-                    // ReSharper disable ExplicitCallerInfoArgument
-                    OnPropertyChanged(nameof(CurrentHeader));
-                    OnPropertyChanged(nameof(CurrentContent));
-                    // ReSharper restore ExplicitCallerInfoArgument
-                }
-            }
-
-            public int Count => _items.Count;
-            public string CurrentIndexText => $"{SelectedIndex + 1} of {Count}";
-            public object CurrentHeader => _items[SelectedIndex].header;
-            public object CurrentContent => _items[SelectedIndex].content;
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        public class MyCompletionData : ICompletionData
-        {
-            public MyCompletionData(string text)
-            {
-                Text = text;
-            }
-
-            public IImage Image => null;
-
-            public string Text { get; }
-
-            // Use this property if you want to show a fancy UIElement in the list.
-            public object Content => _contentControl ??= BuildContentControl();
-
-            public object Description => "Description for " + Text;
-
-            public double Priority { get; } = 0;
-
-            public void Complete(TextArea textArea, ISegment completionSegment,
-                EventArgs insertionRequestEventArgs)
-            {
-                textArea.Document.Replace(completionSegment, Text);
-            }
-
-            Control BuildContentControl()
-            {
-                TextBlock textBlock = new TextBlock();
-                textBlock.Text = Text;
-                textBlock.Margin = new Thickness(5);
-
-                return textBlock;
-            }
-
-            Control _contentControl;
-        }
-
-        class ElementGenerator : VisualLineElementGenerator, IComparer<Pair>
-        {
-            public List<Pair> controls = new List<Pair>();
-
-            /// <summary>
-            /// Gets the first interested offset using binary search
-            /// </summary>
-            /// <returns>The first interested offset.</returns>
-            /// <param name="startOffset">Start offset.</param>
-            public override int GetFirstInterestedOffset(int startOffset)
-            {
-                int pos = controls.BinarySearch(new Pair(startOffset, null), this);
-                if (pos < 0)
-                    pos = ~pos;
-                if (pos < controls.Count)
-                    return controls[pos].Key;
-                else
-                    return -1;
-            }
-
-            public override VisualLineElement ConstructElement(int offset)
-            {
-                int pos = controls.BinarySearch(new Pair(offset, null), this);
-                if (pos >= 0)
-                    return new InlineObjectElement(0, controls[pos].Value);
-                else
-                    return null;
-            }
-
-            int IComparer<Pair>.Compare(Pair x, Pair y)
-            {
-                return x.Key.CompareTo(y.Key);
-            }
-        }
-
-        private void InsertSnippetButton_Click(object sender, RoutedEventArgs e)
-        {
-            var className = new SnippetReplaceableTextElement { Text = "Name" };
-            var snippet = new Snippet
-            {
-                Elements =
-                {
-                    new SnippetTextElement { Text = "public class " },
-                    className,
-                    new SnippetTextElement
-                    {
-                        Text = "\n{\n    public "
-                    },
-                    new SnippetBoundElement { TargetElement = className },
-                    new SnippetTextElement { Text = "()\n    {\n        " },
-                    new SnippetCaretElement(),
-                    new SnippetTextElement { Text = "\n    }\n}" }
-                }
-            };
-
-            snippet.Insert(_textEditor.TextArea);
-            _textEditor.Focus();
-        }
     }
 }
