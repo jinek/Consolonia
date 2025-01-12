83b63cdd6e7f52de2782719f3fbe76070d109eee
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-01-12 12:04:43 -0800

    fix template

--- src/Tools/Consolonia.Templates/ConsoloniaAppTemplate/App.axaml.cs
+++ src/Tools/Consolonia.Templates/ConsoloniaAppTemplate/App.axaml.cs
@@ -1,10 +1,10 @@
-using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Consolonia;
 
 namespace ConsoloniaAppTemplate;
 
-public partial class App : Application
+public partial class App : ConsoloniaApplication
 {
     public override void Initialize()
     {
