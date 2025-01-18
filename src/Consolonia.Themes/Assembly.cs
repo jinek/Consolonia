72e043b941d1d7b24601e833c657827a0874f665
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-01-18 15:28:50 -0800

    implement custom consolonia xmlns (#266)
    Add namespace atttributes and xmlns definitions

--- src/Consolonia.Themes/Assembly.cs
+++ src/Consolonia.Themes/Assembly.cs
@@ -1,3 +1,7 @@
 using System;
+using Avalonia.Metadata;
 
 [assembly: CLSCompliant(false)] //todo: should we make it compliant?
+
+[assembly: XmlnsDefinition("https://github.com/jinek/consolonia", "Consolonia.Themes")]
+[assembly: XmlnsDefinition("https://github.com/jinek/consolonia", "Consolonia.Themes.Templates.Controls.Helpers")]
