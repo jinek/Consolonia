57e9555dfcd2e298e6cc768b74089b4e56c3ffd3
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-02-04 19:44:07 -0800

    add expander first cut with gallery page

---
+++ src/Consolonia.Gallery/Gallery/GalleryViews/GalleryExpander.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Consolonia.Gallery.Gallery.GalleryViews
+{
+    public partial class GalleryExpander : UserControl
+    {
+        public GalleryExpander()
+        {
+            InitializeComponent();
+        }
+    }
+}
