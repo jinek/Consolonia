2152cf650b3d8a9a99646f66824def91b1632116
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-02-05 18:36:05 -0800

    Expander control (#287)
    * add expander 
    * add carets to togglebutton and expander

---
+++ src/Consolonia.Gallery/Gallery/GalleryViews/GalleryGridSplitter.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Consolonia.Gallery.Gallery.GalleryViews
+{
+    public partial class GalleryGridSplitter : UserControl
+    {
+        public GalleryGridSplitter()
+        {
+            InitializeComponent();
+        }
+    }
+}
