5e5cd32e027cd94a3428956e9f9910cf6f5bb8ff
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-02-06 16:37:12 -0800

    add relativepanel gallery page

---
+++ src/Consolonia.Gallery/Gallery/GalleryViews/GalleryRelativePanel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Consolonia.Gallery.Gallery.GalleryViews
+{
+    public partial class GalleryRelativePanel : UserControl
+    {
+        public GalleryRelativePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
