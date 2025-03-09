cae413637962acfb93e99a8e4b919e7bc285f093
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-03-09 16:07:31 -0700

    add a windows test and messagebox test

---
+++ src/Tests/Consolonia.Gallery.Tests/MessageBoxTests.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Consolonia.Gallery.Tests.Base;
+using Consolonia.NUnit;
+using NUnit.Framework;
+
+namespace Consolonia.Gallery.Tests
+{
+    [TestFixture]
+    internal class MessageBoxTests : GalleryTestsBaseBase
+    {
+        [Test]
+        public async Task PerformSingleTest()
+        {
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Enter); // show messagebox
+            await Task.Delay(100); // animation
+            await UITest.AssertHasText("OK/Cancel Message box", "Do you want to");
+            await UITest.AssertHasText("🗕", "🗖", "🗙");
+            await UITest.AssertHasNoText("🗗");
+            await UITest.KeyInput(Key.Escape);
+            await Task.Delay(100);  // animation
+            foreach (var x in new string[] { "OK/Cancel Message box", "Do you want to", "🗕", "🗖", "🗙" })
+                await UITest.AssertHasNoText(x);
+        }
+    }
+}
---
+++ src/Tests/Consolonia.Gallery.Tests/WindowsTests.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Consolonia.Gallery.Tests.Base;
+using Consolonia.NUnit;
+using NUnit.Framework;
+
+namespace Consolonia.Gallery.Tests
+{
+    [TestFixture]
+    internal class WindowsTests : GalleryTestsBaseBase
+    {
+        [Test]
+        public async Task PerformSingleTest()
+        {
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Down); // change startup to be centerowner
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Enter); // add window
+            await Task.Delay(100); // animation
+            await UITest.AssertHasText("New Window 1");
+            await UITest.AssertHasText("🗕", "🗖", "🗙");
+            await UITest.AssertHasNoText("🗗");
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Enter); // close button
+            await Task.Delay(100);  // animation
+            foreach (var x in new string[] { "New Window 1", "DialogResult:", "🗕", "🗖", "🗙" })
+                await UITest.AssertHasNoText(x);
+        }
+    }
+}
