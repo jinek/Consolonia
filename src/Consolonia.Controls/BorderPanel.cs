70e5cb3afcca677cf0f5c5cceffb44606248337f
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-08-15 08:49:10 -0700

    snapshot with panel as root

---
+++ src/Consolonia.Controls/BorderPanel.cs
@@ -0,0 +1,113 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.LogicalTree;
+using Avalonia.Metadata;
+using Consolonia.Controls.Brushes;
+
+namespace Consolonia.Controls;
+
+/// <summary>
+/// BorderPanel is a control that combines a Panel and a Border.
+/// </summary>
+/// <remarks>
+/// * It removes a lot of copy-pasta border templates making it easier for a theme to control the border appearance for popups.
+/// * It is smart about LineBrushes with Edge style, applying background to non-edge borders and no background to edge borders.
+/// * It automatically crops the margin of the panel when hosted in a popup to avoid gaps between the button and the popup.
+/// </remarks>
+[TemplatePart(PART_Panel, typeof(Panel))]
+[TemplatePart(PART_Border, typeof(Border))]
+public class BorderPanel : TemplatedControl
+{
+    private const string PART_Panel = "PART_Panel";
+    private const string PART_Border = "PART_Border";
+    private Panel _panel;
+    private Border _border;
+
+    public static readonly StyledProperty<object> ContentProperty =
+     AvaloniaProperty.Register<BorderPanel, object>(nameof(Content));
+
+
+    public BorderPanel()
+    {
+        ContentControl
+    }
+
+    [Content]
+    [TemplateContent]
+    public object Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        this._panel = e.NameScope.Find<Panel>(PART_Panel);
+        this._border = e.NameScope.Find<Border>(PART_Border);
+        var popup = this.FindLogicalAncestorOfType<Popup>();
+        if (popup != null)
+        {
+            popup.PropertyChanged += Popup_PropertyChanged;
+        }
+    }
+
+    private void Popup_PropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == Popup.IsOpenProperty)
+        {
+            if (HasEdgeStyle())
+            {
+                var popup = sender as Popup;
+                if (popup != null && (bool)e.NewValue)
+                {
+                    // Automatically crop the margin to get rid of gap between button and popup
+                    switch (popup?.Placement)
+                    {
+                        case PlacementMode.Top:
+                        case PlacementMode.TopEdgeAlignedLeft:
+                        case PlacementMode.TopEdgeAlignedRight:
+                            _panel.Margin = new Thickness(0, 0, 0, -1);
+                            break;
+                        case PlacementMode.Bottom:
+                        case PlacementMode.BottomEdgeAlignedRight:
+                        case PlacementMode.BottomEdgeAlignedLeft:
+                            _panel.Margin = new Thickness(0, -1, 0, 0);
+                            break;
+                        case PlacementMode.Left:
+                        case PlacementMode.LeftEdgeAlignedTop:
+                        case PlacementMode.LeftEdgeAlignedBottom:
+                            _panel.Margin = new Thickness(0, 0, -1, 0);
+                            break;
+                        case PlacementMode.Right:
+                        case PlacementMode.RightEdgeAlignedTop:
+                        case PlacementMode.RightEdgeAlignedBottom:
+                            _panel.Margin = new Thickness(-1, 0, 0, 0);
+                            break;
+                        default:
+                            // Do nothing for other placements
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private bool HasEdgeStyle()
+    {
+        var lineBrush = _border?.BorderBrush as LineBrush;
+        return (lineBrush != null &&
+            (lineBrush.LineStyle.Left == LineStyle.Edge ||
+             lineBrush.LineStyle.Top == LineStyle.Edge ||
+             lineBrush.LineStyle.Right == LineStyle.Edge ||
+             lineBrush.LineStyle.Bottom == LineStyle.Edge ||
+             lineBrush.LineStyle.Left == LineStyle.EdgeWide ||
+             lineBrush.LineStyle.Top == LineStyle.EdgeWide ||
+             lineBrush.LineStyle.Right == LineStyle.EdgeWide ||
+             lineBrush.LineStyle.Bottom == LineStyle.EdgeWide));
+    }
+}
