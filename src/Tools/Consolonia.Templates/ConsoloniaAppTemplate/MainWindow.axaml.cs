879b3e7d081e31d4913e09776d829a9784ac1cb6
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-08-17 11:49:31 -0700

    cleanup template to align with Avalonia new app template.

--- src/Tools/Consolonia.Templates/ConsoloniaAppTemplate/MainWindow.axaml.cs
+++ src/Tools/Consolonia.Templates/ConsoloniaAppTemplate/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 
 namespace ConsoloniaAppTemplate
 {
@@ -7,6 +10,12 @@ namespace ConsoloniaAppTemplate
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnExit(object sender, RoutedEventArgs e)
+        {
+            var lifetime = Application.Current!.ApplicationLifetime as IControlledApplicationLifetime;
+            lifetime!.Shutdown();
         }
     }
 }
