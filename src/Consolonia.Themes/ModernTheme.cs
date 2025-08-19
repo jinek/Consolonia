0b68bef0f9332848fae3111d38ec58657d552083
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-08-19 11:05:52 -0700

    Rename Fluent theme to ModernTheme (#399)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/Consolonia.Themes/ModernTheme.cs
@@ -0,0 +1,19 @@
+using System;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Consolonia.Themes
+{
+    public class ModernTheme : Styles
+    {
+        public ModernTheme(IServiceProvider sp = null)
+        {
+            AvaloniaXamlLoader.Load(sp, this);
+        }
+    }
+
+    [Obsolete("FluentTheme is deprecated. Use ModernTheme instead.")]
+    public class FluentTheme : ModernTheme
+    {
+    }
+}
