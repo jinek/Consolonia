5550df05fe631288a4ab317eb6c54c168014295b
Evgeny Gorbovoy <jinek@msn.com>
2025-08-27 12:14:34 +0200

    Add XML documentation for TurboVision themes

--- src/Consolonia.Themes/TurboVisionDarkTheme.cs
+++ src/Consolonia.Themes/TurboVisionDarkTheme.cs
@@ -4,6 +4,9 @@ using Avalonia.Styling;
 
 namespace Consolonia.Themes
 {
+    /// <summary>
+    /// Dark theme based on Turbo Vision templates. Compatible with 16-fg / 8-bg ANSI
+    /// </summary>
     public class TurboVisionDarkTheme : Styles
     {
         public TurboVisionDarkTheme(IServiceProvider sp = null)
--- src/Consolonia.Themes/TurboVisionTheme.cs
+++ src/Consolonia.Themes/TurboVisionTheme.cs
@@ -4,6 +4,9 @@ using Avalonia.Styling;
 
 namespace Consolonia.Themes
 {
+    /// <summary>
+    /// A theme that mimics the look and feel of Turbo Vision applications. Compatible with 16-fg / 8-bg ANSI
+    /// </summary>
     public class TurboVisionTheme : Styles
     {
         public TurboVisionTheme(IServiceProvider sp = null)
