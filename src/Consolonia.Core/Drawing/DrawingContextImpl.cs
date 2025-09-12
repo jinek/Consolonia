036b9adf167d66c5a2c01ad39cafefa0aebaed0a
Evgeny Gorbovoy <jinek@msn.com>
2025-09-12 21:11:21 +0200

    one more attempt

--- src/Consolonia.Core/Drawing/DrawingContextImpl.cs
+++ src/Consolonia.Core/Drawing/DrawingContextImpl.cs
@@ -898,6 +898,7 @@ namespace Consolonia.Core.Drawing
         /// <returns>foreground color</returns>
         private static Color GetForegroundColorForQuadPixel(SKColor[] pixelColors, char quadPixel)
         {
+            // ReSharper disable once DuplicateCode
             if (pixelColors.Length != 4)
                 throw new ArgumentException($"{nameof(pixelColors)} must have 4 elements.");
 
@@ -935,6 +936,7 @@ namespace Consolonia.Core.Drawing
         /// <returns></returns>
         private static Color GetBackgroundColorForQuadPixel(SKColor[] pixelColors, char quadPixel)
         {
+            // ReSharper disable once DuplicateCode
             SKColor skColor = quadPixel switch
             {
                 ' ' => CombineColors(pixelColors.ToArray()),
