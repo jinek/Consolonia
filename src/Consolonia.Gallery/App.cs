2a6de179439241c33da3a22c9ef0af85daa13db3
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-10-03 10:58:38 -0700

    Use AvaloniaEdit for ViewCode dialog

--- src/Consolonia.Gallery/App.cs
+++ src/Consolonia.Gallery/App.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml.Styling;
 using Consolonia.Themes;
 
 namespace Consolonia.Gallery
@@ -25,6 +27,8 @@ namespace Consolonia.Gallery
                 Styles.Add(new ModernTheme());
             else
                 Styles.Add(new TurboVisionTheme());
+
+            Styles.Add(new StyleInclude(new Uri($"avares://Consolonia.AvaloniaEdit")) { Source = new Uri("Theme.axaml", UriKind.Relative) });
 
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
                 desktop.MainWindow = new MainWindow(); // designer runs as classic desktop
---
+++ src/Consolonia.Gallery/View/CodeDialog.axaml.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+using AvaloniaEdit;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.TextMate;
+using Iciclecreek.Avalonia.WindowManager;
+using TextMateSharp.Grammars;
+
+namespace Consolonia.Gallery.View
+{
+    public partial class CodeDialog : ManagedWindow
+    {
+        private TextEditor _textEditor;
+        private RegistryOptions _registryOptions;
+        private TextMate.Installation _textMateInstallation;
+
+        public CodeDialog(string file, string text)
+        {
+            InitializeComponent();
+
+            this.Title = file;
+
+            _textEditor = this.FindControl<TextEditor>("Editor")!;
+            _textEditor.Document = new TextDocument(text);
+            ThemeName theme = Application.Current.RequestedThemeVariant == ThemeVariant.Dark ? 
+                ThemeName.VisualStudioDark : 
+                ThemeName.VisualStudioLight;
+            _registryOptions = new RegistryOptions(theme);
+            _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
+            Language language = _registryOptions.GetLanguageByExtension(Path.GetExtension(file));
+            string scopeName = _registryOptions.GetScopeByLanguageId(language.Id);
+            _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(language.Id));
+            TextMateInstallationOnAppliedTheme(this, _textMateInstallation);
+        }
+
+        private void TextMateInstallationOnAppliedTheme(object sender, TextMate.Installation e)
+        {
+            ApplyBrushAction(e, "editor.background", brush => _textEditor.Background = brush);
+            ApplyBrushAction(e, "editor.foreground", brush => _textEditor.TextArea.Foreground = brush);
+
+            if (!ApplyBrushAction(e, "editor.selectionBackground",
+                    brush => _textEditor.TextArea.SelectionBrush = brush))
+            {
+                if (!ApplyBrushAction(e, "editor.selectionHighlightBackground",
+                        brush => _textEditor.TextArea.SelectionBrush = brush))
+                {
+                    if (Application.Current!.TryGetResource("TextAreaSelectionBrush", out var resourceObject))
+                    {
+                        if (resourceObject is IBrush brush)
+                        {
+                            _textEditor.TextArea.SelectionBrush = brush;
+                        }
+                    }
+                }
+            }
+
+            if (!ApplyBrushAction(e, "editor.lineHighlightBackground",
+                    brush =>_textEditor.TextArea.TextView.CurrentLineBackground = brush))
+            {
+                _textEditor.TextArea.TextView.SetDefaultHighlightLineColors();
+            }
+
+            //Todo: looks like the margin doesn't have a active line highlight, would be a nice addition
+            if (!ApplyBrushAction(e, "editorLineNumber.foreground",
+                    brush => _textEditor.LineNumbersForeground = brush))
+            {
+                _textEditor.LineNumbersForeground = _textEditor.TextArea.Foreground;
+            }
+            _textEditor.TextArea.TextView.CurrentLineBorder = new Pen(Brushes.Transparent, thickness: 0);
+        }
+        bool ApplyBrushAction(TextMate.Installation e, string colorKeyNameFromJson, Action<IBrush> applyColorAction)
+        {
+            if (!e.TryGetThemeColor(colorKeyNameFromJson, out var colorString))
+                return false;
+
+            if (!Color.TryParse(colorString, out Color color))
+                return false;
+
+            var colorBrush = new SolidColorBrush(color);
+            applyColorAction(colorBrush);
+            return true;
+        }
+
+    }
+}
