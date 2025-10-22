8b62e31e7042f98a60775c10e4287833674cb8bd
Evgeny Gorbovoy <jinek@msn.com>
2025-10-22 16:23:45 +0200

    Replace `Medo.X11Clipboard` with `jinek.X11Clipboard.Fork` for X11 clipboard functionality.

--- src/Consolonia.PlatformSupport/Clipboard/X11Clipboard.cs
+++ src/Consolonia.PlatformSupport/Clipboard/X11Clipboard.cs
@@ -12,18 +12,18 @@ namespace Consolonia.PlatformSupport.Clipboard
         {
             await base.ClearAsync();
 
-            Medo.X11.X11Clipboard.Clipboard.Clear();
+            jinek.X11.X11Clipboard.Clipboard.Clear();
         }
 
         public override Task<string> GetTextAsync()
         {
-            string text = Medo.X11.X11Clipboard.Clipboard.GetText();
+            string text = jinek.X11.X11Clipboard.Clipboard.GetText();
             return Task.FromResult(text);
         }
 
         public override Task SetTextAsync(string text)
         {
-            Medo.X11.X11Clipboard.Clipboard.SetText(text ?? string.Empty);
+            jinek.X11.X11Clipboard.Clipboard.SetText(text ?? string.Empty);
             return Task.CompletedTask;
         }
     }
