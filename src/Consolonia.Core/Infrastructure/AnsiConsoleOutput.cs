1a425b34e714e887fda3cdcd71de5849d22a4755
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-10-22 07:53:09 -0700

    bad merge

--- src/Consolonia.Core/Infrastructure/AnsiConsoleOutput.cs
+++ src/Consolonia.Core/Infrastructure/AnsiConsoleOutput.cs
@@ -97,8 +97,8 @@ namespace Consolonia.Core.Infrastructure
                     ushort glyphWidth = glyph.MeasureText();
                     if (glyphWidth > 1)
                     {
-                        WriteText(new string(' ', textWidth - 1));
                         WriteText(Esc.SetCursorPosition(bufferPoint.X + 1, bufferPoint.Y));
+                        WriteText(new string(' ', textWidth - 1));
                     }
 
                     WriteText(Esc.SetCursorPosition(bufferPoint.X, bufferPoint.Y));
