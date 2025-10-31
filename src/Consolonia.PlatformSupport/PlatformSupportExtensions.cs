96eb5f56262d74017a04d04620ec3601df7bac06
Evgeny Gorbovoy <jinek@msn.com>
2025-10-31 06:16:20 +0100

    internal clipboard for linux when x11 is not available

--- src/Consolonia.PlatformSupport/PlatformSupportExtensions.cs
+++ src/Consolonia.PlatformSupport/PlatformSupportExtensions.cs
@@ -79,7 +79,10 @@ namespace Consolonia
                 else
                     // alternatively use xclip CLI tool
                     //return builder.With<IClipboard>(new XClipClipboard());
+                if(Environment.GetEnvironmentVariable("DISPLAY")==":0")
                     clipboardImpl = new X11Clipboard();
+                else
+                    clipboardImpl = new ConsoleClipboard();
             }
             else
             {
