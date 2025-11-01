fb4d4a3d9e85ba20300429060d5d50c10b147562
Evgeny Gorbovoy <jinek@msn.com>
2025-11-01 03:26:38 +0100

    ConsoleWindow does not create a console and should not dispose it, but I don't know how to do better with AvaloniaLocator.

--- src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
+++ src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
@@ -470,6 +470,8 @@ namespace Consolonia.Core.Infrastructure
                     Console.MouseEvent -= ConsoleOnMouseEvent;
                     Console.FocusEvent -= ConsoleOnFocusEvent;
                     _ = DirtyRegions.GetSnapshotAndClear();
+                    if(Console is IDisposable disposableConsole)
+                        disposableConsole.Dispose();
                     _singletonGuard = false;
                 }
             }
