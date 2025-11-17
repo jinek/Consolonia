897affca0c9f2c41be8a225f17c9f54cc81ab9e8
Evgeny Gorbovoy <jinek@msn.com>
2025-11-17 02:27:26 +0100

    Improve crash handling by introducing `CrashViaThreadPool` to ensure proper application termination when dialogs fail.

--- src/Experimental/Edit.NET/App.error.cs
+++ src/Experimental/Edit.NET/App.error.cs
@@ -10,12 +10,11 @@ namespace EditNET
     public partial class App
     {
         /// <summary>
-        ///     Avalonia rethrows an exception to unobserved task, which we also handles. Thus marking this exception to know it
-        ///     was handled already
-        ///     Why not just to show it from UnobservedTask? Because we don't know when that will happen (finalizer call)
+        /// Avalonia rethrows an exception to unobserved task, which we also handles. Thus marking this exception to know it was handled already
+        /// Why not just to show it from UnobservedTask? Because we don't know when that will happen (finalizer call)
         /// </summary>
         internal static readonly object HandledExceptionHackMark = new();
-
+        
         private static void HandleDispatcherExceptions()
         {
 #if HANDLE_CRASH
@@ -31,9 +30,8 @@ namespace EditNET
         }
 
         /// <summary>
-        ///     Try to show a message box with the error and suggest to restart the application.
-        ///     If the dialog is not closed within 30 seconds or if it's throws an exception the application will do a regular
-        ///     crash
+        /// Try to show a message box with the error and suggest to restart the application.
+        /// If the dialog is not closed within 30 seconds or if it's throws an exception the application will do a regular crash
         /// </summary>
         /// <param name="exception">Exception to report</param>
         internal static async Task ShowApplicationError(Exception exception)
@@ -42,32 +40,50 @@ namespace EditNET
 
             var tsc = new CancellationTokenSource();
             var wrappedException = new CrashAppException(exception);
-
+            
             _ = Task.Delay(TimeSpan.FromSeconds(30), tsc.Token)
                 .ContinueWith(_ =>
                     {
                         // this does not write exception to output: Environment.FailFast(null, exception);
-                        ThreadPool.QueueUserWorkItem(_ => throw wrappedException);
-                    }, CancellationToken.None,
+                        // crashing the application because dialog can not be shown
+                        CrashViaThreadPool();
+                    },CancellationToken.None,
                     TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Default);
             try
             {
                 await Dispatcher.UIThread.InvokeAsync(async () =>
                 {
-                    //todo: we need better dialog
-                    await MessageBox.ShowDialog("Application Error",
-                        string.Concat("We apologize for the inconvenience, but an unexpected error occurred. \r\n",
-                            "Please restart the application or leave this dialog open - it will close the application automatically in 30 seconds.\r\n",
-                            "Error details: ", exception.ToString()));
+                    try
+                    {
+                        //todo: we need better dialog
+                        await MessageBox.ShowDialog("Application Error",
+                            string.Concat("We apologize for the inconvenience, but an unexpected error occurred. \r\n",
+                                "Please restart the application or leave this dialog open - it will close the application automatically in 30 seconds.\r\n",
+                                "Error details: ", exception.ToString()));
+                    }
+                    catch
+                    {
+                        // crashing the application because dialog can not be shown
+                        CrashViaThreadPool();
+                        throw;
+                    }
                 });
             }
             catch
             {
-                // crashing the main thread, it will write exception to outputs
-                throw wrappedException;
+                // crashing the application because dialog can not be shown
+                CrashViaThreadPool();
+                throw;
             }
 
             await tsc.CancelAsync();
+            
+            return;
+
+            void CrashViaThreadPool()
+            {
+                ThreadPool.QueueUserWorkItem(_ => throw wrappedException);
+            }
         }
     }
 }
