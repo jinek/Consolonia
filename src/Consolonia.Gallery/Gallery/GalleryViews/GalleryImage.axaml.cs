779714bee1d2635ce8201bfbf35d26298a29ae73
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-11-19 18:27:50 -0800

    add ability to load and view an image

--- src/Consolonia.Gallery/Gallery/GalleryViews/GalleryImage.axaml.cs
+++ src/Consolonia.Gallery/Gallery/GalleryViews/GalleryImage.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 
 namespace Consolonia.Gallery.Gallery.GalleryViews
 {
@@ -7,6 +11,35 @@ namespace Consolonia.Gallery.Gallery.GalleryViews
         public GalleryImage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            IStorageProvider storageProvider = TopLevel.GetTopLevel(this).StorageProvider;
+            if (storageProvider.CanOpen)
+            {
+                IStorageFolder startLocation =
+                    await storageProvider.TryGetFolderFromPathAsync(Environment.CurrentDirectory);
+                IReadOnlyList<IStorageFile> files = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "Open image",
+                    AllowMultiple = false,
+                    SuggestedStartLocation = startLocation,
+                    FileTypeFilter = new List<FilePickerFileType>
+                    {
+                        new("Image files") { Patterns = ["*.jpg", "*.jpeg", "*.png"] },
+                        new("*.* files") { Patterns = ["*.*"] },
+                    }
+                });
+
+                var file = files.FirstOrDefault();
+                if (file != null)
+                {
+                    BigImage.Source = new Avalonia.Media.Imaging.Bitmap(file.Path.LocalPath);
+                    BigImage.IsVisible = true;
+                    WrapPanel.IsVisible = false;
+                }
+            }
         }
     }
 }
