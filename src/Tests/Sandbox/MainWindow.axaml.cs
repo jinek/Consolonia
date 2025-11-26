e8c9dc1a132ef5079388f2c1c0d6020cf2ce62e1
Evgeny Gorbovoy <jinek@msn.com>
2025-11-26 13:58:23 +0100

    Revert sandbox

--- src/Tests/Sandbox/MainWindow.axaml.cs
+++ src/Tests/Sandbox/MainWindow.axaml.cs
@@ -1,16 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace Sandbox
 {
     public partial class MainWindow : Window
     {
-        public int[] sequences = [0, 1, 2, 3];
-        private int _iteration = 0;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -23,11 +19,6 @@ namespace Sandbox
 
             var lifetime = Application.Current!.ApplicationLifetime as IControlledApplicationLifetime;
             lifetime!.Shutdown();
-        }
-
-        private void InputElement_OnKeyDown(object? sender, KeyEventArgs e)
-        {
-            overlapTxt.Margin = new Thickness(sequences[_iteration++ % sequences.Length], 0, 0, 0);
         }
     }
 }
