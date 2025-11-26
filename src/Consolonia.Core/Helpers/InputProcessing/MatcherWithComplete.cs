7b49b27994169f0867ee1a0fad688dfbc6a9bcbc
Evgeny Gorbovoy <jinek@msn.com>
2025-11-26 03:05:18 +0100

    Removing NLog dependency from Core (#551)

--- src/Consolonia.Core/Helpers/InputProcessing/MatcherWithComplete.cs
+++ src/Consolonia.Core/Helpers/InputProcessing/MatcherWithComplete.cs
@@ -1,12 +1,12 @@
 using System;
+using Avalonia.Logging;
 using Consolonia.Core.Helpers.Logging;
-using NLog;
 
 namespace Consolonia.Core.Helpers.InputProcessing
 {
     public abstract class MatcherWithComplete<T, TComplete> : IMatcher<T>
     {
-        private readonly ILogger _logger = Log.CreateInputLogger();
+        private readonly ParametrizedLogger _logger = Log.CreateInputLogger(LogEventLevel.Information);
 
         protected MatcherWithComplete(Action<TComplete> onComplete)
         {
@@ -23,7 +23,7 @@ namespace Consolonia.Core.Helpers.InputProcessing
 
         protected void Complete(TComplete input)
         {
-            _logger.Info($"Complete fired for {GetDebugInfo()} with input: {input}");
+            _logger.Log2($"Complete fired for {GetDebugInfo()} with input: {input}");
             OnComplete.Invoke(input);
         }
     }
--- src/Consolonia.Core/Helpers/Logging/Log.cs
+++ src/Consolonia.Core/Helpers/Logging/Log.cs
@@ -1,17 +1,71 @@
-using NLog;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Avalonia.Logging;
 
 namespace Consolonia.Core.Helpers.Logging
 {
     internal static class Log
     {
-        public static ILogger Create(LogCategory category)
+        public static ParametrizedLogger Create(LogCategory category, LogEventLevel level)
         {
-            return LogManager.GetLogger("Consolonia." + category);
+            string areaName = "Consolonia." + category;
+            ParametrizedLogger? parametrizedLogger = Logger.TryGet(level, areaName);
+            return parametrizedLogger != null
+                ? (ParametrizedLogger)parametrizedLogger!
+                : new ParametrizedLogger(new NullSink(), level, areaName);
         }
 
-        public static ILogger CreateInputLogger()
+        public static ParametrizedLogger CreateInputLogger(LogEventLevel level)
         {
-            return Create(LogCategory.Input);
+            return Create(LogCategory.Input, level);
+        }
+
+
+        public static void Log2(this ParametrizedLogger logger, string message,
+            [CallerFilePath] string sourcePath = null)
+        {
+            string sourceName = Path.GetFileName(sourcePath ?? "default");
+            logger.Log(sourceName, message);
+        }
+
+        public static void Log2<T>(this ParametrizedLogger logger, string messageTemplate, T parameter,
+            [CallerFilePath] string sourcePath = null)
+        {
+            string sourceName = Path.GetFileName(sourcePath ?? "default");
+            logger.Log(sourceName, messageTemplate, parameter);
+        }
+
+        public static void Log3<T0, T1>(this ParametrizedLogger logger, string messageTemplate, T0 propertyValue0,
+            T1 propertyValue1,
+            [CallerFilePath] string sourcePath = null)
+        {
+            string sourceName = Path.GetFileName(sourcePath ?? "default");
+            logger.Log(sourceName, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public static void Log3<T0, T1, T2>(this ParametrizedLogger logger, string messageTemplate, T0 propertyValue0,
+            T1 propertyValue1, T2 propertyValue2,
+            [CallerFilePath] string sourcePath = null)
+        {
+            string sourceName = Path.GetFileName(sourcePath ?? "default");
+            logger.Log(sourceName, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        internal class NullSink : ILogSink
+        {
+            public bool IsEnabled(LogEventLevel level, string area)
+            {
+                return false;
+            }
+
+            public void Log(LogEventLevel level, string area, object source, string messageTemplate)
+            {
+            }
+
+            public void Log(LogEventLevel level, string area, object source, string messageTemplate,
+                params object[] propertyValues)
+            {
+            }
         }
     }
 }
